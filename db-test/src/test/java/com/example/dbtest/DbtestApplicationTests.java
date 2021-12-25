dbd98582549f9c84113d0db2d220596aa8f504f3
Heedong Kim <ruthetum@gmail.com>
2021-12-25 19:05:39 +0900

    db test 세팅

---
+++ db-test/src/test/java/com/example/dbtest/DbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
